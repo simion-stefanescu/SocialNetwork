30f64ffdb7b8a4c2b5850649a44999f6dddb3211
simion-stefanescu <32930426+simion-stefanescu@users.noreply.github.com>
2019-10-15 20:23:04 +0300

    Reset password Functionalityfully implemented

--- app/src/main/java/com/example/socialnetwork/ResetPasswordActivity.java
+++ app/src/main/java/com/example/socialnetwork/ResetPasswordActivity.java
@@ -1,10 +1,22 @@
 package com.example.socialnetwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class ResetPasswordActivity extends AppCompatActivity {
@@ -13,12 +25,15 @@ public class ResetPasswordActivity extends AppCompatActivity {
 
     private Button ResetPasswordSendEmailButton;
     private EditText ResetEmailInput;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mToolbar = (Toolbar) findViewById(R.id.forget_password_toolbar);
         setSupportActionBar(mToolbar);
@@ -28,5 +43,38 @@ public class ResetPasswordActivity extends AppCompatActivity {
 
         ResetPasswordSendEmailButton = (Button) findViewById(R.id.reset_password_email_button);
         ResetEmailInput = (EditText) findViewById(R.id.reset_password_EMAIL);
+
+        ResetPasswordSendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = ResetEmailInput.getText().toString();
+
+                if(TextUtils.isEmpty(userEmail)){
+
+                    Toast.makeText(ResetPasswordActivity.this, "Please write avalid E-mail address", Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+
+                                Toast.makeText(ResetPasswordActivity.this, "Please check our e-maik", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                            }else {
+
+                                String message = task.getException().getMessage();
+                                Toast.makeText(ResetPasswordActivity.this, "Error Occured: " + message, Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+
+                }
+            }
+        });
     }
 }
